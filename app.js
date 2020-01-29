f0e99009b98a61989f76e08641eaf0d27a4e6dd2
Ovidiu Fosalau <ovidiu.fosalau@gmail.com>
2020-01-29 12:39:52 +0200

    added the functions for the game (skeleton) and created the attack function

--- app.js
+++ app.js
@@ -6,10 +6,46 @@ new Vue({
         isGameRunning: false
     },
     methods: {
-        startGame: function() {
+        startGame: function () {
             this.isGameRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
+        },
+        attack: function () {
+            let max = 10;
+            let min = 3;
+            // damage is the maximum between the minimum damage and
+            // the calculated random damage (up until the max dmg)
+            let damage = Math.max(min, Math.floor(Math.random() * max) + 1);
+            this.monsterHealth -=damage;
+
+            if(this.monsterHealth <=0){
+                alert('You won!');
+                this.isGameRunning = false;
+                this.monsterHealth = 0;
+                return;
+            }
+
+            max = 12;
+            min = 5;
+            damage = Math.max(min, Math.floor(Math.random() * max) + 1);
+            this.playerHealth -= damage;
+
+            if(this.playerHealth <=0){
+                alert('You lost!');
+                this.isGameRunning = false;
+                this.playerHealth = 0;
+                return;
+            }
+        },
+        specialAttack: function () {
+
+        },
+        heal: function () {
+
+        },
+        giveUp: function () {
+
         }
     }
 });
